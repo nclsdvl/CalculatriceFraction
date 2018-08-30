480b0ebfb6edc9eb91d493a5aafdfdf0998df956
nclsdvl <nicolas.duval@maformationenligne.bzh>
2018-08-30 23:28:13 +0200

    pgcd ok

--- src/Nico/ReductionPGCD.java
+++ src/Nico/ReductionPGCD.java
@@ -6,7 +6,7 @@ public class ReductionPGCD {
     public static int numerateurSoluce = Fraction.getNumerateurSoluce();
     public static int denominateurSoluce = Fraction.getDenominateurSoluce();
 
-    public static int modulo_B_A = denominateurSoluce % numerateurSoluce;
+
     public static int PGCD;
     public static int swap;
 
@@ -18,23 +18,35 @@ public class ReductionPGCD {
     public static int CalculPGCD( int numerateurSoluce, int denominateurSoluce) {
         for (int i = 0; i < numerateurSoluce && i < denominateurSoluce; i++) {
             int modulo_A_B = numerateurSoluce % denominateurSoluce;
+            int modulo_B_A = denominateurSoluce % numerateurSoluce;
+            if (numerateurSoluce > denominateurSoluce) {
+                if (modulo_A_B == 0) {
 
-            if (numerateurSoluce > denominateurSoluce && modulo_A_B == 0) {
+                    PGCD = denominateurSoluce;
+                    System.out.println( "le PGCD = "+PGCD);
+                    return PGCD;
+                } else {
+                    swap = denominateurSoluce;
+                    numerateurSoluce = swap;
+                    denominateurSoluce = modulo_A_B;
 
-                PGCD = denominateurSoluce;
-            } else {
-                swap = denominateurSoluce;
-                numerateurSoluce = swap;
-                denominateurSoluce = modulo_A_B;
-
+                }
             }
 
-            if (numerateurSoluce < denominateurSoluce ||modulo_B_A == 0) {
-                PGCD = numerateurSoluce;
-            } else {
-                denominateurSoluce = numerateurSoluce;
-                numerateurSoluce = modulo_B_A;
-            }
+            else {
+                if (numerateurSoluce < denominateurSoluce) {
+                    if (modulo_B_A == 0) {
+                        PGCD = numerateurSoluce;
+                        System.out.println( "le PGCD = "+PGCD);
+                        return PGCD;
+                    } else {
+                        swap = numerateurSoluce;
+                        denominateurSoluce = numerateurSoluce;
+                        numerateurSoluce = modulo_B_A;
+                    }
+
+                    }
+                }
         }
 
         System.out.println( "le PGCD = "+PGCD);
